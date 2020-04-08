a8b9a38bc92a1c139a17fd03c3eec59bbc4f299d
Jean Rauwers <jeanrauwers@gmail.com>
2020-04-08 12:44:46 +0100

    Add express server and serving the client

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/client.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on: http://localhost:${port}`)
+})
