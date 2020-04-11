084ec651107e81f56e56978fa7f3a75d1953be0b
Jean Rauwers <jeanrauwers@gmail.com>
2020-04-11 23:33:30 +0100

    removing console.log

--- src/keyboard-listener.js
+++ src/keyboard-listener.js
@@ -4,13 +4,10 @@ export default function createKeyBoardListener(document) {
     }
 
     function subscribe(observerFunction) {
-        console.log('subscribe event')
         state.observers.push(observerFunction)
     }
 
     function notifyAll(command) {
-        console.log('notified event')
-
         for (const observerFunction of state.observers) {
             observerFunction(command)
         }
@@ -20,9 +17,6 @@ export default function createKeyBoardListener(document) {
 
     function keyDownHandler(event) {
         const keyPressed = event.key
-
-        console.log('keyDown', keyPressed)
-
         const command = {
             playerId: 'player1',
             keyPressed
